7af1dba2ede59d608a54287bd8e9d3dd2edecb6c
gonchi.sanchez@gmail.com <gonchi.sanchez@gmail.com@9a82c1f5-93ec-ef82-32a2-313a8e265d22>
2012-02-24 21:37:11 +0000

    agrego los archivos que faltaban

---
+++ trunk/ANTICIPACION.vhd
@@ -0,0 +1,34 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ANTICIPACION is
+port(Rt,Rs : in std_logic_vector(4 downto 0);
+	  EX_MEM_Rd,MEM_WB_Rd : in std_logic_vector(4 downto 0);
+	  EscrReg_ExMem, EscrReg_MemWb: in std_logic;
+	  Out_A, Out_B : out std_logic_vector(1 downto 0)
+);	  
+end ANTICIPACION;
+
+architecture Behavioral of ANTICIPACION is
+
+begin
+
+	Out_A<= "10" when (EscrReg_ExMem = '1' and EX_MEM_Rd /= "00000" and EX_MEM_Rd = Rs ) else
+			  "01" when (EscrReg_MemWb = '1' and MEM_WB_Rd /= "00000" and MEM_WB_Rd = Rs and EX_MEM_Rd /= Rs) else
+			  "00";
+			  
+	Out_B<= "10" when (EscrReg_ExMem = '1' and EX_MEM_Rd /= "00000" and EX_MEM_Rd = Rt ) else
+			  "01" when (EscrReg_MemWb = '1' and MEM_WB_Rd /= "00000" and MEM_WB_Rd = Rt and EX_MEM_Rd /= Rt ) else
+			  "00";
+
+
+end Behavioral;
+
---
+++ trunk/Unidad_Deteccion_Riesgos.vhd
@@ -0,0 +1,33 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Unidad_Deteccion_Riesgos is
+	port(Rt_IFID,Rs_IFID, Rt_IDEX: in std_logic_vector(4 downto 0);
+		  LeerMem: in std_logic;
+		  EscrPc,EscrIFID, Salida_mux: out std_logic	
+		);
+end Unidad_Deteccion_Riesgos;
+
+architecture Behavioral of Unidad_Deteccion_Riesgos is
+
+begin
+
+	Salida_mux <= '1' when (LeerMem = '1' and (Rt_IDEX = Rs_IFID or Rt_IDEX = Rt_IFID)) else
+					  '0'	;
+					  
+	EscrPc <= '0' when (LeerMem = '1' and (Rt_IDEX = Rs_IFID or Rt_IDEX = Rt_IFID)) else
+				 '1';
+
+   EscrIFID <= '0' when (LeerMem = '1' and (Rt_IDEX = Rs_IFID or Rt_IDEX = Rt_IFID)) else
+				   '1';				 
+
+end Behavioral;
+
